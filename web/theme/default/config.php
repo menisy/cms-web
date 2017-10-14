f5820e794ddf1996828f12671dc3c7381bf5f5a4
menisy <elmenisy@gmail.com>
2017-10-14 23:34:13 +0200

    remove reshow welcome

--- web/theme/default/config.php
+++ web/theme/default/config.php
@@ -8,7 +8,7 @@
  * Xibo is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
- * any later version. 
+ * any later version.
  *
  * Xibo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,7 +17,7 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with Xibo.  If not, see <http://www.gnu.org/licenses/>.
- */ 
+ */
 
 /*
  * Theme Configuration Object
